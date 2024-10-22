066e341e958395b9f0bc0b56eea137cfa79f0259
hugoeljugo <hugougena27@gmail.com>
2024-10-22 14:06:02 +0000

    add: datetime to posts

--- models/post.py
+++ models/post.py
@@ -1,4 +1,5 @@
 from sqlmodel import Field, SQLModel
+from datetime import datetime
 
 class PostBase(SQLModel):
     text: str
@@ -7,9 +8,11 @@ class PostBase(SQLModel):
 class Post(PostBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="user.id", index=True)
+    date: datetime | None = Field(default=datetime.now())
 
 class PostPublic(PostBase):
     id: int
+    date: datetime
 
 class PostCreate(PostBase):
     user_id: int
