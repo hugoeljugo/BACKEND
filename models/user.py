cb109d069246305613cafdfce7140c66e01f67b4
hugoeljugo <hugougena27@gmail.com>
2024-11-12 15:47:59 +0000

    add: follower relationship??

--- models/user.py
+++ models/user.py
@@ -1,4 +1,8 @@
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
+
+class UserLink(SQLModel, table=True):
+    follower_id: int | None = Field(default=None, foreign_key="user.id", primary_key=True)
+    followed_id: int | None = Field(default=None, foreign_key="user.id", primary_key=True)
 
 class UserBase(SQLModel):
     username: str = Field(index=True)
@@ -9,6 +13,10 @@ class User(UserBase, table=True):
     password: str = Field(index=True)
     pfp: bytes | None = Field(default=None)
     disabled: bool | None = Field(default=False)
+
+    follows: list["User"] = Relationship(back_populates="followed_by", link_model=UserLink, sa_relationship_kwargs={'foreign_keys':[UserLink.follower_id]})
+
+    followed_by: list["User"] = Relationship(back_populates="follows", link_model=UserLink, sa_relationship_kwargs={'foreign_keys':[UserLink.followed_id]})
 
 class UserPublic(UserBase):
     id: int
